a93df0219f4d9e44df64fdba5b2f0f1a25055dde
Nikita Uzakov <nikitauzakov2@gmail.com>
2020-08-27 16:58:57 +0400

    update prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
 import React, { FunctionComponent } from "react";
+import {Auth} from './Auth';
+// import {newTaskForm} from './newTaskForm';
+// import {tasks} from './tasks';
 
 export const App: FunctionComponent () => {
-    
+    return (
+        <div className="app">
+					<Auth />
+        </div>
+    )
 }
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,22 @@
-import { combineReducers } from "redux";
-import { reducer as formReducer } from "redux-form";
-import * as actions from "../actions/index";
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import * as actions from '../actions/index';
+import { handleActions } from 'redux-actions';
+const auth = handleActions(
+  {
+    [actions.fetchRegisterSuccess](state, { payload: { credentials } }) {
+      return {
+        ...credentials
+      };
+    }
+  },
+  {}
+);
+
 export default combineReducers({
-  taskRemovingState,
-  taskFetchingState,
-  tasks,
-  form: formReducer,
+  // taskRemovingState,
+  // taskFetchingState,
+  // tasks,
+  auth,
+  form: formReducer
 });
